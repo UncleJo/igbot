2bfa74fb888400531c82d14b8bfca8ec54493b91
unclejo <sanxchep@gmail.com>
2020-05-30 23:34:57 +0530

    Pypi Update 0.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'igbot',
   packages = ['igbot'],
-  version = '0.2',
+  version = '0.2.1',
   license='MIT',
   description = 'This Package will help you to automate Instagram Tasks. It uses selenium.',
   author = 'Sanxchep Sharma',
   author_email = 'sanxchep@gmail.com',
   url = 'https://github.com/UncleJo/igbot',
-  download_url = 'https://github.com/UncleJo/igbot/archive/v0.2.tar.gz',
+  download_url = 'https://github.com/UncleJo/igbot/archive/v0.2.1.tar.gz',
   keywords = ['Instagram', 'Automation', 'Bot', 'Selenium'], 
   install_requires=[
           'selenium'
